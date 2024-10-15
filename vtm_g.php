21941f7005e54193b67346ef90c58e54bad599cb
ietzel <67884277+ietzel@users.noreply.github.com>
2024-10-15 15:36:54 -0400

    Update vtm_g.php

--- vtm_g.php
+++ vtm_g.php
@@ -95,7 +95,6 @@ echo '</html>';
 
 $conn = new mysqli($servername, $username, $password);
 
-// Check connection
 if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
