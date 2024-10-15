f68b10b58a94c43a54c53ce887f3e55049781627
ietzel <67884277+ietzel@users.noreply.github.com>
2024-10-15 15:36:34 -0400

    Update and rename vtm_g.html to vtm_g.php

--- vtm_g.php
+++ vtm_g.php
@@ -92,4 +92,12 @@ echo 'loop();';
 echo '</script>';
 echo '</body>';
 echo '</html>';
+
+$conn = new mysqli($servername, $username, $password);
+
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
 ?>
