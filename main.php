3e0c2bf1164569c5de47e9c0c2164dc1eea9be4d
ietzel <67884277+ietzel@users.noreply.github.com>
2024-10-16 17:56:17 -0400

    Update main.php

--- main.php
+++ main.php
@@ -1,13 +1,6 @@
-
-'/*Physical: Athletics, Brawl, Craft, Drive, Firearms, Larceny, Melee, Stealth, Survival.';
-'Mental: Academics, Awareness, Finance, Investigation, Medicine, Occult, Politics, Science, Technology.';
-'Social: Animal Ken, Etiquette, Insight, Intimidation, Leadership, Performace, Persuasion, Streetwise, Subterfuge.*/';
-
-
+<?php
 $conn = new mysqli($servername, $username, $password);
-
 if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
-
 ?>
