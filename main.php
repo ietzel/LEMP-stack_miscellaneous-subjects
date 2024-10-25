4501644062549c670182de66ec5aa0f3738cf77f
ietzel <67884277+ietzel@users.noreply.github.com>
2024-10-25 17:24:38 -0400

    Update main.php

--- main.php
+++ main.php
@@ -1,6 +1,18 @@
-<?php
-$conn = new mysqli($servername, $username, $password);
-if ($conn->connect_error) {
-  die("Connection failed: " . $conn->connect_error);
-}
+<?php 
+    $servername = "localhost"; 
+    $username = "root"; 
+    $password = ""; 
+  
+    $conn = new mysqli($servername, $username, $password); 
+    if ($conn->connect_error) { 
+          die("Connection failed: " . $conn->connect_error); 
+    } 
+  
+    $sql = "CREATE DATABASE gfgDB"; 
+    if ($conn->query($sql) === TRUE) { 
+          echo "Database has been created successfully"; 
+    } else { 
+          echo "Error creating database: " . $conn->error; 
+    } 
+    $conn->close(); 
 ?>
